739b40e31d34857f41cdf779b2466e0f6996eb62
Nikolas Philips <nikolas.philips@baloise.com>
2021-06-28 02:03:25 +0200

    Add short containerBuild impl

--- vars/containerBuild.groovy
+++ vars/containerBuild.groovy
@@ -3,5 +3,11 @@ def call(input) {
     if (!input) {
         input = [:]
     }
-    this.sh 'echo "Building Container"'
+    this.container(name: 'buildah') {
+        this.withVault(configuration: [timeout: 60, vaultCredentialId: 'vault_token', vaultUrl: 'http://vault:8200'], vaultSecrets: [[path: '/secret/registry', secretValues: [[envVar: 'USERNAME', vaultKey: 'username'], [envVar: 'PASSWORD', vaultKey: 'password']]]]) {
+            this.sh 'buildah login -u $USERNAME -p $PASSWORD registry.baloise.dev'
+        }
+        this.sh 'buildah bud -f $(pwd)/Dockerfile -t registry.baloise.dev/' + input.repository
+        this.sh 'buildah push registry.baloise.dev/test/buildah-test' + input.repository
+    }
 }
