504391c665a26d427d5a7da168e6602386eb26da
Nikolas Philips <nikolas.philips@baloise.com>
2021-07-22 23:00:15 +0200

    Small variable changes

--- vars/containerBuild.groovy
+++ vars/containerBuild.groovy
@@ -7,7 +7,8 @@ def call(input) {
         this.withVault(configuration: [timeout: 60, vaultCredentialId: 'vault_token', vaultUrl: 'http://vault:8200'], vaultSecrets: [[path: '/secret/registry', secretValues: [[envVar: 'USERNAME', vaultKey: 'username'], [envVar: 'PASSWORD', vaultKey: 'password']]]]) {
             this.sh 'buildah login -u $USERNAME -p $PASSWORD registry.baloise.dev'
         }
-        this.sh 'buildah bud -f $(pwd)/Dockerfile -t registry.baloise.dev/' + input.repository
-        this.sh 'buildah push registry.baloise.dev/' + input.repository
+        def imageName = 'registry.baloise.dev/' + input.repository
+        this.sh 'buildah bud -f $(pwd)/Dockerfile -t ' + imageName
+        this.sh 'buildah push ' + imageName
     }
 }
