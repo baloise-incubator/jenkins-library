1fbdf9f80aef3acead246c5077d68f1ba1b79f79
Nikolas Philips <nikolas.philips@baloise.com>
2022-10-23 12:54:03 +0200

    Add patchYaml

--- vars/patchYaml.groovy
+++ vars/patchYaml.groovy
@@ -1,14 +1,12 @@
 import com.baloise.jenkinslibrary.common.Registry
-import com.baloise.jenkinslibrary.gitops.DeploymentResult
 import com.baloise.jenkinslibrary.gitops.GitopsApi
 
-DeploymentResult call(input) {
+def call(input) {
     if (!input) {
         input = [:]
     }
     Registry registry = new Registry(this)
-    GitopsApi gitops = registry.getApi(GitopsApi)
-
+    GitopsApi gitops = registry.getApi(GitopsApi.class)
     gitops.deploy(input.stage, input.organisation, input.repositoryName,
             input.file, input.yamlPatches, input.singleCommit,
             input.createPullRequest, input.commitMessage, input.jira)
